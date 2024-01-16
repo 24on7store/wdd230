7c5d227025c2b7cf12c5fdb5e3f43638c8d238a9
24on7store <132136928+24on7store@users.noreply.github.com>
2024-01-17 00:53:05 +0300

    change

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+var currentYear = new Date().getFullYear();
+
+var copyrightParagraph = document.getElementById("copyright");
+copyrightParagraph.innerHTML = + currentYear;
+
+var lastModifiedParagraph = document.getElementById('lastModified');
+var lastModifiedDate = document.lastModified;
+lastModifiedParagraph.innerHTML = 'Last Modified' + lastModifiedDate;
